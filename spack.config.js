c165fd9a5dec12c4ef25db960759f7b4f139b134
Coop <coop.coding@gmail.com>
2022-05-07 19:06:31 +1000

    Initial commit

---
+++ spack.config.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+const { config } = require('@swc/core/spack')
+
+module.exports = config({
+  entry: {
+    index: path.join(__dirname, 'src', 'index.js'),
+  },
+  output: {
+    path: path.join(__dirname, 'out'),
+  },
+  options: {
+    sourceMaps: false,
+  },
+})
